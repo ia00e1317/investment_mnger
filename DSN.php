a717938e099674033118f660919bc6dcec8a54c2
ia00e1317 <56835708+ia00e1317@users.noreply.github.com>
2020-11-02 22:32:12 +0900

    Update DSN.php

--- DSN.php
+++ DSN.php
@@ -1,12 +1,12 @@
 <?php
 
 // DB接続情報
-define('user', 'tress');
-define('pass', 'miyamasuzaka1234');
-define('dsn', 'mysql:host=mysql643.db.sakura.ne.jp;dbname=tress_murayama;charset=utf8');
+define('user', '●●●');
+define('pass', '●●●●●');
+define('dsn', 'mysql:host=mysql643.db.sakura.ne.jp;dbname=●●●●●;charset=utf8');
 
 	define('encryptKey', 'tress');
-	define('log_path', '/home/tress/www/investmng/log/debug.log');
+	define('log_path', '/home/●●●/www/investmng/log/debug.log');
 
 function db_connect() {
 	$pdo = new PDO(dsn, user, pass, array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
